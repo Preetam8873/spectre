cda41d121ac27dce8fe27a52a9e5f21dcbabcbd1
Preetam8873 <87963092+Preetam8873@users.noreply.github.com>
2025-03-25 15:09:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3060,3 +3060,8 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+  if (window.location.pathname.endsWith(".html")) {
+    let newUrl = window.location.pathname.replace(".html", "") + window.location.search;
+    history.replaceState(null, "", newUrl);
+  }
+
